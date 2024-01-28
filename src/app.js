9d56c26dd7b5981486b54b6cf9f51d3d057cc803
SonyJais <135392807+SonyJais@users.noreply.github.com>
2024-01-28 13:42:55 +0100

    axios added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
+function refreshWeather(response) {
+  let tempVal = document.querySelector(".temp");
+  let temperature = response.data.temperature.current;
+  tempVal.innerHTML = Math.round(temperature);
+
+  let moistureVal = document.querySelector("#humidity");
+  let humidVal = response.data.temperature.humidity;
+  moistureVal.innerHTML = Math.round(humidVal);
+
+  let speedVal = document.querySelector("#windSpeed");
+  let windSpeedVal = response.data.wind.speed;
+  speedVal.innerHTML = Math.round(windSpeedVal, 2);
+}
+
+function searchCity(city) {
+  let apiKey = "aa56bt976def310e73o04395caed4d33";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
@@ -7,7 +27,7 @@ function buttonClick(event) {
   let searchInput = document.querySelector(".search-input");
   let cityNameInput = document.querySelector(".cityName");
   cityNameInput.innerHTML = capitalizeFirstLetter(searchInput.value);
-  console.log(cityNameInput);
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector(".search-form");
